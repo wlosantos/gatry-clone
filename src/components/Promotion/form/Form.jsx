0173ba32415b158cdc92aa45f72a8d0f958c0e68
Wendel Lopes <wendelopes@msn.com>
2021-07-03 21:58:22 -0300

    Implemented update form

--- src/components/Promotion/form/Form.jsx
+++ src/components/Promotion/form/Form.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import './form.scss'
@@ -11,9 +11,18 @@ const initialValue = {
   price: ''
 }
 
-const PromotionForm = () => {
-  const [values, setValues] = useState(initialValue)
+const PromotionForm = ({ id }) => {
+  const [values, setValues] = useState(id ? null : initialValue)
   const history = useHistory()
+
+  useEffect(() => {
+    if(id) {
+      axios.get(`http://localhost:5000/promotions/${id}`)
+        .then( response => {
+          setValues(response.data)
+        })
+    }
+  }, [])
 
   function onChange(event) {
     const {name, value} = event.target
@@ -24,7 +33,12 @@ const PromotionForm = () => {
   function onSubmit(event) {
     event.preventDefault()
 
-    axios.post('http://localhost:5000/promotions', values)
+    const method = id ? 'put': 'post'
+    const url = id 
+      ? `http://localhost:5000/promotions/${id}`
+      : 'http://localhost:5000/promotions' 
+
+    axios[method](url, values)
       .then(response => {
         history.push('/')
       })
@@ -34,44 +48,51 @@ const PromotionForm = () => {
     <div>
       <h1>Promo Show</h1>
       <h2>Nova Promoção</h2>
-
-      <form onSubmit={onSubmit}>
-        <div className="promotion-form__group">
-          <label htmlFor="title">Título</label>
-          <input type="text" 
-            id="title"
-            name='title'
-            onChange={onChange}
-          />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="url">Link</label>
-          <input type="text" 
-            id="url"
-            name='url'
-            onChange={onChange}
-          />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="imageUrl">Image (URL)</label>
-          <input type="text" 
-            id="imageUrl"
-            name='imageUrl'
-            onChange={onChange}
-          />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="price">Price</label>
-          <input type="number" 
-            id="price"
-            name='price'
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <button type='submit'>Salvar</button>
-        </div>
-      </form>
+      { !values 
+      ? ( <div>Carregando...</div>) 
+      : (
+          <form onSubmit={onSubmit}>
+            <div className="promotion-form__group">
+              <label htmlFor="title">Título</label>
+              <input type="text" 
+                id="title"
+                name='title'
+                onChange={onChange}
+                value={values.title}
+              />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="url">Link</label>
+              <input type="text" 
+                id="url"
+                name='url'
+                onChange={onChange}
+                value={values.url}
+              />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="imageUrl">Image (URL)</label>
+              <input type="text" 
+                id="imageUrl"
+                name='imageUrl'
+                onChange={onChange}
+                value={values.imageUrl}
+              />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="price">Price</label>
+              <input type="number" 
+                id="price"
+                name='price'
+                onChange={onChange}
+                value={values.price}
+              />
+            </div>
+            <div>
+              <button type='submit'>Salvar</button>
+            </div>
+          </form>
+      )}
     </div>
   )
 }
