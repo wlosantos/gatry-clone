234d7a6eeb3edf5ffd8cf35cfcd2ff60c6e93361
Wendel Lopes <wendelopes@msn.com>
2021-07-05 16:31:40 -0300

    Adding sub comments

--- src/components/Promotion/modal/CommentsTree.jsx
+++ src/components/Promotion/modal/CommentsTree.jsx
@@ -1,7 +1,40 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './CommentsTree.scss'
 
+function getTree(list) {
+  if(!list) return []
+
+  const roots = []
+  const childrenByParentId = {}
+
+  list.forEach( item => {
+    if (!item.parentId) {
+      roots.push(item)
+      return;
+    }
+
+    if(!childrenByParentId[item.parentId]) {
+      childrenByParentId[item.parentId] = []
+    }
+
+    childrenByParentId[item.parentId].push(item)
+  })
+
+  function buildNodes(nodes) {
+    
+    if (!nodes) return null
+
+    return nodes.map( node => ({
+      ...node,
+      children: buildNodes(childrenByParentId[node.id])
+    }))
+  }
+
+  return buildNodes(roots)
+}
+
 const CommentsTree = ({comments, sendComment}) => {
+  const tree = useMemo(() => getTree(comments), [comments])
   const [comment, setComment] = useState('')
   const [activeCommentBox, setActiveCommentBox] = useState(false)
 
@@ -9,43 +42,53 @@ const CommentsTree = ({comments, sendComment}) => {
     return <div>Carregando...</div>
   }
 
-  return (
-    <ul className='promotion-modal-comments-tree'>
-      {comments.map( item => (
-        <li key={item.id}>
-          <img src={item.user.avatarUrl} alt={`foto de ${item.user.name}`} className='avatar' />
-          <div className='info'>
-            <span>{item.user.name}</span>
-            <p>{item.comment}</p>
-            <button 
-              type='button' 
-              className='answer-button'
-              onClick={() => {
-                setComment('')
-                setActiveCommentBox(activeCommentBox=== item.id ? null : item.id)}
-                }>
-                Responder
+  function renderItem (item) {
+    return (
+      <li key={item.id}>
+        <img src={item.user.avatarUrl} alt={`foto de ${item.user.name}`} className='avatar' />
+        <div className='info'>
+          <span>{item.user.name}</span>
+          <p>{item.comment}</p>
+          <button 
+            type='button' 
+            className='answer-button'
+            onClick={() => {
+              setComment('')
+              setActiveCommentBox(activeCommentBox=== item.id ? null : item.id)}
+              }>
+              Responder
+            </button>
+          {activeCommentBox === item.id && (
+            <div className="comment-box">
+              <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+              <button 
+                type='button' 
+                className='send-button'
+                onClick={() => {
+                  sendComment(comment, item.id)
+                  setComment('')
+                  setActiveCommentBox(null)
+                }}
+              >
+                Enviar
               </button>
-            {activeCommentBox === item.id && (
-              <div className="comment-box">
-                <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
-                <button 
-                  type='button' 
-                  className='send-button'
-                  onClick={() => {
-                    sendComment(comment, item.id)
-                    setComment('')
-                    setActiveCommentBox(null)
-                  }}
-                >
-                  Enviar
-                </button>
-              </div>
-            )}
-          </div>
-        </li>
-      ))}
+            </div>
+          )}
+          {item.children && renderList(item.children)}
+        </div>
+      </li>
+    )
+  }
+  function renderList (list) {
+    return (
+      <ul className='promotion-modal-comments-tree'>
+      {list.map( item => renderItem(item))}
     </ul>
+    )
+  }
+
+  return (
+    renderList(tree)
   )
 }
 
